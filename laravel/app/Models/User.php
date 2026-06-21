8e384bb3d8c8004a503bdd591e8a84b1881e1064
Novokshon <novoksonovm@gmail.com>
2026-06-21 18:37:08 +0500

    Added readme and hope fixed all bugs

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -72,6 +72,6 @@ class User extends Authenticatable
      */
     public function orders()
     {
-        return $this->hasMany(Order::class, 'user_id');
+        return $this->hasMany(Order::class, 'buyer_id');
     }
 }
